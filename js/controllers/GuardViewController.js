949eff05b5c0879c10f581e8dd3c26edbf169767
David Shustin <david.shustin@gmail.com>
2018-08-19 12:39:49 -0400

    Add some stuff

--- js/controllers/GuardViewController.js
+++ js/controllers/GuardViewController.js
@@ -1,5 +1,20 @@
 app.controller('GuardViewController', ['$scope', function($scope) {
     $scope.activeKiosks = ['Sally1', 'Sally2', 'Joe1', 'Joe2', 'Bob1', 'Bob2'];
+    $scope.sallyActive = true;
+    $scope.joeActive = true;
+    $scope.bobActive = true;
+    $scope.calculateActiveKiosks = function() {
+        $scope.activeKiosks = [];
+        if ($scope.sallyActive) {
+            $scope.activeKiosks.push('Sally1', 'Sally2');
+        }
+        if ($scope.joeActive) {
+            $scope.activeKiosks.push('Joe1', 'Joe2');
+        }
+        if ($scope.bobActive) {
+            $scope.activeKiosks.push('Bob1', 'Bob2');
+        }
+    }
     $scope.isWide = function() {
         return $scope.activeKiosks.length <= 3;
     };
